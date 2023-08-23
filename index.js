ffab42b766213545e2af77c9b3aae83eac74c0a1
Shinjini Sen <98477105+shinjinisen@users.noreply.github.com>
2023-08-23 16:05:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,27 +6,28 @@ for(var i=0;i<n;i++)
         var buttonInnerHTML = this.innerHTML;
         switch(buttonInnerHTML)
         {
-            case 'w':var audio= new Audio("./sounds/crash.mp3"); 
-                     audio.play();
+            case 'w':var audio1= new Audio("./sounds/crash.mp3"); 
+                     audio1.play();
                      break;
-            case 'a':var audio= new Audio("./sounds/kick-bass.mp3"); 
-                     audio.play();
+            case 'a':var audio2= new Audio("./sounds/kick-bass.mp3"); 
+                     audio2.play();
                      break;
-            case 's':var audio= new Audio("./sounds/snare.mp3"); 
-                     audio.play();
+            case 's':var audio3= new Audio("./sounds/snare.mp3"); 
+                     audio3.play();
                      break;
-            case 'd':var audio= new Audio("./sounds/tom-1.mp3"); 
-                     audio.play();
+            case 'd':var audio4= new Audio("./sounds/tom-1.mp3"); 
+                     audio4.play();
                      break;
-            case 'j':var audio= new Audio("./sounds/tom-2.mp3"); 
-                     audio.play();
+            case 'j':var audio5= new Audio("./sounds/tom-2.mp3"); 
+                     audio5.play();
                      break;
-            case 'k':var audio= new Audio("./sounds/tom-3.mp3"); 
-                     audio.play();
+            case 'k':var audio6= new Audio("./sounds/tom-3.mp3"); 
+                     audio6.play();
                      break;
-            case 'l':var audio= new Audio("./sounds/tom-4.mp3"); 
-                     audio.play();
+            case 'l':var audio7= new Audio("./sounds/tom-4.mp3"); 
+                     audio7.play();
                      break;
+            default: break;
         }
        
         
