76a4aa84cfabc37263f55a51169bf212e69f0e15
Shinjini Sen <98477105+shinjinisen@users.noreply.github.com>
2023-08-23 18:02:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,34 +4,50 @@ for(var i=0;i<n;i++)
     document.querySelectorAll("button")[i].addEventListener("click", f1);
     function f1() {
         var buttonInnerHTML = this.innerHTML;
-        switch(buttonInnerHTML)
-        {
-            case 'w':var audio1= new Audio("./sounds/crash.mp3"); 
-                     audio1.play();
-                     break;
-            case 'a':var audio2= new Audio("./sounds/kick-bass.mp3"); 
-                     audio2.play();
-                     break;
-            case 's':var audio3= new Audio("./sounds/snare.mp3"); 
-                     audio3.play();
-                     break;
-            case 'd':var audio4= new Audio("./sounds/tom-1.mp3"); 
-                     audio4.play();
-                     break;
-            case 'j':var audio5= new Audio("./sounds/tom-2.mp3"); 
-                     audio5.play();
-                     break;
-            case 'k':var audio6= new Audio("./sounds/tom-3.mp3"); 
-                     audio6.play();
-                     break;
-            case 'l':var audio7= new Audio("./sounds/tom-4.mp3"); 
-                     audio7.play();
-                     break;
-            default: console.log(buttonInnerHTML);
-        }
-       
-        
+        makeSound(buttonInnerHTML);
+        buttonAnimation(buttonInnerHTML);
     }
-    
 }
 
+
+document.addEventListener("keydown",function(event){
+    makeSound(event.key);
+    buttonAnimation(event.key);
+});
+
+function makeSound(key)
+{
+    switch(key)
+    {
+        case 'w':var audio1= new Audio("./sounds/crash.mp3"); 
+                 audio1.play();
+                 break;
+        case 'a':var audio2= new Audio("./sounds/kick-bass.mp3"); 
+                 audio2.play();
+                 break;
+        case 's':var audio3= new Audio("./sounds/snare.mp3"); 
+                 audio3.play();
+                 break;
+        case 'd':var audio4= new Audio("./sounds/tom-1.mp3"); 
+                 audio4.play();
+                 break;
+        case 'j':var audio5= new Audio("./sounds/tom-2.mp3"); 
+                 audio5.play();
+                 break;
+        case 'k':var audio6= new Audio("./sounds/tom-3.mp3"); 
+                 audio6.play();
+                 break;
+        case 'l':var audio7= new Audio("./sounds/tom-4.mp3"); 
+                 audio7.play();
+                 break;
+        default: console.log(key);
+    }
+}
+
+function buttonAnimation(currentKey){
+    var active=document.querySelector("."+currentKey);
+    active.classList.add("pressed");
+    setTimeout(function(){
+        active.classList.remove("pressed");},100);
+    
+}
